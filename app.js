c0d2fbdba1ff7e7f077c7d935c922af6e3a40a24
Jason Lin <tongyu0418@gmail.com>
2019-09-19 19:53:53 +0000

    add mongoatlas

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost:27017/yelp-camp_v11", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://lttymm:lttyuu55@ycamp-oie6x.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true});
+	
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
